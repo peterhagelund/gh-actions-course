723c3b5cf1e42788c4bdd4583b55bb9c5b821e2c
Peter Hagelund <peterhagelund@mac.com>
2025-08-08 15:09:16 -0400

    feat(17-custom-actions): first end-to-end complete version including token

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,27 +1,15 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\./]+$/.test(branchName)
 const validateDirectoryName = ({ directoryName }) => /^[a-zA-Z0-9_\-/]+$/.test(directoryName)
 
 async function run() {
-  /*
-  [DONE]1. Parse inputs:
-    1.2 base-branch from which to check for updates
-    1.2 target-branch to use to create PR
-    1.3 GitHub token for auth purposes
-    1.4 Working directory for which to check for dependencies
-  [DONE]2. Execute npm update withing working directory
-  3. Check whether there are modified package*.json files
-  4. If there are modified files:
-    4.1 Add and commit files to the target-branch
-    4.2 Create a PR to the base-brach using the octokit API
-  5. Otherwise conclude the custom action
-  */
-  const baseBranch = core.getInput('base-branch');
-  const targetBranch = core.getInput('target-branch');
-  const gitHubToken = core.getInput('github-token');
-  const workingDirectory = core.getInput('working-directory');
+  const baseBranch = core.getInput('base-branch', { required: true });
+  const targetBranch = core.getInput('target-branch', { required: true });
+  const gitHubToken = core.getInput('github-token', { required: true });
+  const workingDirectory = core.getInput('working-directory', { required: true });
   const debug = core.getBooleanInput('debug');
   core.setSecret(gitHubToken);
   if (!validateBranchName({ branchName: baseBranch })) {
@@ -36,27 +24,46 @@ async function run() {
     core.setFailed('Invalid working-directory name');
     return;
   }
+  const execOptions = {
+    cwd: workingDirectory
+  };
   core.info(`[js-dependecy-update]: base-branch is ${baseBranch}`);
   core.info(`[js-dependecy-update]: target-branch is ${targetBranch}`);
   core.info(`[js-dependecy-update]: working-directory is ${workingDirectory}`);
-  const exitCode = await exec.exec('npm update', [], {
-    cwd: workingDirectory
-  });
+  const exitCode = await exec.exec('npm update', [], execOptions);
   if (exitCode != 0) {
     core.setFailed(`[js-dependency-update]: command "npm update" failed with exit code ${exitCode}`);
     return;
   }
-  const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
-    cwd: workingDirectory
-  });
+  const gitStatus = await exec.getExecOutput(`git status -s package*.json`, [], execOptions);
   if (gitStatus.exitCode != 0) {
     core.setFailed(`[js-dependency-update]: command "git status -s" failed with exit code ${exitCode}`);
     return;
   }
-  if (gitStatus.stdout.length > 0) {
-    core.info('[js-dependecy-update]: There are updates available');
-  } else {
-    core.info('[js-dependecy-update]: There are no updates at this time');
+  if (gitStatus.stdout.length == 0) {
+    core.info(`[js-dependency-update]: There are no updates at this time`);
+    return;
+  }
+  core.info('[js-dependency-update]: There are updates available');
+  await exec.exec(`git config --global user.name=automation`);
+  await exec.exec(`git config --global user.email=automation@nowhere.org`);
+  await exec.exec(`git checkout -b ${targetBranch}`, [], execOptions);
+  await exec.exec(`git add package.json package-lock.json`, [], execOptions);
+  await exec.exec(`git commit -m "chore: update dependencies"`, [], execOptions);
+  await exec.exec(`git push -u origin ${targetBranch} --force`, [], execOptions);
+  const octokit = github.getOctokit(gitHubToken);
+  try {
+    await octokit.rest.pulls.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title: `Update NPM dependencies`,
+      body: `This PR updates the NPM packages`,
+      base: baseBranch,
+      head: targetBranch
+    });
+  } catch (e) {
+    core.error(e.message);
+    core.setFailed(`[js-dependency-update]: Failed to create PR`);
   }
 }
 
