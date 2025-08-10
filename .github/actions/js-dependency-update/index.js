2ce4ad426da6e834e905d204b974a44ee2584aae
Peter Hagelund <peterhagelund@mac.com>
2025-08-10 09:04:26 -0400

    feat(17-custom-actions): add logging abstraction

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 
-const name = 'js-dependency-update';
 const configureGit = async () => {
   await exec.exec(`git config --global user.name automation`);
   await exec.exec(`git config --global user.email automation@nowhere.org`);
@@ -25,11 +24,6 @@ const getInputs = () => {
   if (!validateDirectoryName({ directoryName: workingDirectory })) {
     throw new Error(`The working-directory ${workingDirectory} is invalid`);
   }
-  if (debug) {
-    core.info(`[${name}]: base-branch is ${baseBranch}`);
-    core.info(`[${name}]: target-branch is ${headBranch}`);
-    core.info(`[${name}]: working-directory is ${workingDirectory}`);
-  }
   return {
     baseBranch: baseBranch,
     headBranch: headBranch,
@@ -38,47 +32,55 @@ const getInputs = () => {
     debug: debug
   };
 }
+const setupLogger = ({ debug, prefix } = { debug: false, prefix: '[js-dependency-update]' }) = {
+  debug: (message) => {
+    if (debug) {
+      core.info(`${prefix}[D]: ${message}`);
+    }
+  },
+  info: (message) => {
+    core.error(`${prefix}[I]: ${message}`);
+  },
+  error: (message) => {
+    core.error(`${prefix}[E]: ${message}`);
+  }
+};
 
 async function run() {
-  var inputs;
   try {
-    inputs = getInputs()
-  } catch (e) {
-    setFailed(e.message);
-    return;
-  }
-  const { baseBranch, headBranch, gitHubToken, workingDirectory, debug } = inputs;
-  const execOptions = {
-    cwd: workingDirectory
-  };
-  if (debug) {
-    core.info(`[${name}]: Running "npm update"...`);
-  }
-  const exitCode = await exec.exec('npm update', [], execOptions);
-  if (exitCode != 0) {
-    setFailed(`[${name}]: Command "npm update" failed with exit code ${exitCode}`);
-    return;
-  }
-  if (debug) {
-    core.info(`[${name}]: Running "git status -s package*.json"...`);
-  }
-  const gitStatus = await exec.getExecOutput(`git status -s package*.json`, [], execOptions);
-  if (gitStatus.exitCode != 0) {
-    setFailed(`[${name}]: Command "git status -s" failed with exit code ${exitCode}`);
-    return;
-  }
-  if (gitStatus.stdout.length == 0) {
-    core.info(`[${name}]: There are no updates at this time`);
-    return;
-  }
-  core.info(`[${name}]: There are updates available`);
-  await configureGit();
-  await exec.exec(`git checkout -b ${headBranch}`, [], execOptions);
-  await exec.exec(`git add package.json package-lock.json`, [], execOptions);
-  await exec.exec(`git commit -m "chore: update dependencies"`, [], execOptions);
-  await exec.exec(`git push -u origin ${headBranch} --force`, [], execOptions);
-  const octokit = github.getOctokit(gitHubToken);
-  try {
+    const { baseBranch, headBranch, gitHubToken, workingDirectory, debug } = getInputs();
+    const logger = setupLogger({ debug: debug });
+    logger.debug(`base-branch is ${baseBranch}`);
+    logger.debug(`head-branch is ${headBranch}`);
+    logger.debug(`working-directory is ${workingDirectory}`);
+    const execOptions = {
+      cwd: workingDirectory
+    };
+    logger.debug(`Running "npm update"...`);
+    const exitCode = await exec.exec('npm update', [], execOptions);
+    if (exitCode != 0) {
+      logger.error(`Command "npm update" failed with exit code ${exitCode}`);
+      core.setFailed('npm update failed')
+      return;
+    }
+    logger.debug(`Running "git status -s package*.json"...`);
+    const gitStatus = await exec.getExecOutput(`git status -s package*.json`, [], execOptions);
+    if (gitStatus.exitCode != 0) {
+      logger.error(`Command "git status -s" failed with exit code ${exitCode}`)
+      setFailed('git status failed');
+      return;
+    }
+    if (gitStatus.stdout.length == 0) {
+      logger.info(`There are no updates at this time`);
+      return;
+    }
+    logger.info(`There are updates available`);
+    await configureGit();
+    await exec.exec(`git checkout -b ${headBranch}`, [], execOptions);
+    await exec.exec(`git add package.json package-lock.json`, [], execOptions);
+    await exec.exec(`git commit -m "chore: update dependencies"`, [], execOptions);
+    await exec.exec(`git push -u origin ${headBranch} --force`, [], execOptions);
+    const octokit = github.getOctokit(gitHubToken);
     await octokit.rest.pulls.create({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
@@ -88,8 +90,8 @@ async function run() {
       head: headBranch
     });
   } catch (e) {
-    core.error(e.message);
-    core.setFailed(`[${name}]: Failed to create PR`);
+    setFailed(e.message);
+    return;
   }
 }
 
