65f00712e1ddb94f5d4b357cb4d00009a05b6e72
Peter Hagelund <peterhagelund@mac.com>
2025-08-10 10:28:43 -0400

    feat(17-custom-actions): name fixes and dump env

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,5 +1,9 @@
+from os import environ
 def main():
   print("Hello, World!")
+  gh_env = {n: v for (n, v) in environ if n.startswith("GITHUB_")}
+  for n, v in gh_env.items():
+    print(f"{n} = {v}")
 
 if __name__ == '__main__':
   main()
