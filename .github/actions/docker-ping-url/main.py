489ecf5269a7f7e7aaf22ebdd22c66c936873275
Peter Hagelund <peterhagelund@mac.com>
2025-08-10 11:04:32 -0400

    feat(17-custom-actions): minor refactoring

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -3,13 +3,7 @@ from sys import exit
 from requests import get
 from time import sleep
 
-def main():
-  url = getenv("INPUT_URL")
-  max_attempts = int(getenv("INPUT_MAX_ATTEMPTS"))
-  delay = float(getenv("INPUT_DELAY"))
-  print(f"url = {url}")
-  print(f"max_attempts = {max_attempts}")
-  print(f"delay = {delay}")
+def is_url_reachable(url: str, max_attempts: int, delay: float) -> bool:
   print(f"Pinging URL {url}...")
   try:
     for attempt in range(max_attempts):
@@ -17,16 +11,28 @@ def main():
       response = get(url)
       status_code = response.status_code
       print(f"Status code is {status_code}")
-      if  status_code == 200:
+      if status_code == 200:
         print("Success")
-        exit(0)
+        return True
       print("Sleeping...")
       sleep(delay)
     print("Unable to ping URL successfully")
   except Exception as e:
     print(f"Exception during ping: {e}")
+  return False
+
+def run():
+  url = getenv("INPUT_URL")
+  max_attempts = int(getenv("INPUT_MAX_ATTEMPTS"))
+  delay = float(getenv("INPUT_DELAY"))
+  print(f"url = {url}")
+  print(f"max_attempts = {max_attempts}")
+  print(f"delay = {delay}")
+  reachable = is_url_reachable(url, max_attempts, delay)
+  if reachable:
+    exit(0)
   exit(1)
 
 
 if __name__ == '__main__':
-  main()
+  run()
