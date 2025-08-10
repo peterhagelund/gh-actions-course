507fef5a13a48b2a49de710024884f811474fc50
Peter Hagelund <peterhagelund@mac.com>
2025-08-10 10:41:13 -0400

    feat(17-custom-actions): first cut at failing the action

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,4 +1,6 @@
 from os import getenv
+from sys import exit
+
 def main():
   url = getenv("INPUT_URL")
   max_attempts = getenv("INPUT_MAX-ATTEMPTS")
@@ -6,6 +8,7 @@ def main():
   print(f"url = {url}")
   print(f"max_attempts = {max_attempts}")
   print(f"delay = {delay}")
+  exit(1)
 
 
 if __name__ == '__main__':
